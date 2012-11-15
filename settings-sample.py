b33346bd1cf4bb1e59e93cb3aa202d8c20b18d7d
Nikolai Kristiansen <nikolaik@gmail.com>
2012-11-15 17:38:15 +0100

    With sample config

--- settings-sample.py
+++ settings-sample.py
@@ -6,3 +6,10 @@ DB_HOST = "localhost"       # hostname
 DB_USER = "snurr"           # username
 DB_PASSWORD = ""
 DB_NAME = "snurr"
+
+DISABLE_TETRIS = False
+TETRIS_DB_HOST = "localhost"       # hostname
+TETRIS_DB_USER = "snurr_tetris"    # username
+TETRIS_DB_PASSWORD = ""
+TETRIS_DB_NAME = "paalbra_tetris"
+
